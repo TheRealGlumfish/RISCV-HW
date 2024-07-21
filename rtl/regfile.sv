63e73ab1b7b55bed164968fd1edbb5b6ba39a4b7
Dimitris Alexopoulos <glumfishfish@gmail.com>
2024-07-21 14:01:48 +0300

    Add regfile

---
+++ rtl/regfile.sv
@@ -0,0 +1,32 @@
+`timescale 1ps/1ps
+module regfile(
+    input               clk,
+    input               wen,
+    input        [4:0]  regA_sel,
+    input        [4:0]  regB_sel,
+    input        [4:0]  regW_sel,
+    input        [31:0] regW_i,
+    output logic [31:0] regA_o,
+    output logic [31:0] regB_o
+);
+
+logic [31:0] registers[31:0];
+
+// TODO: Examine write/read order and check synthesis results
+always_comb begin
+    if(regA_sel == 4'b0)
+        regA_o = 0;
+    else
+        regA_o = registers[regA_sel];
+    if(regB_sel == 4'b0)
+        regB_o = 0;
+    else
+        regB_o = registers[regB_sel];
+end
+
+always_ff @(posedge clk) begin
+    if(wen) // TODO: Potentially make zero register unwritable, check synthesis results
+        registers[regW_sel] <= regW_i;
+end
+
+endmodule
---
+++ tb/regfile_tb.sv
@@ -0,0 +1,61 @@
+`timescale 1ps/1ps
+module regfile_tb;
+
+bit          clk;
+bit          wen;
+bit   [4:0]  regA_sel;
+bit   [4:0]  regB_sel;
+bit   [4:0]  regW_sel;
+bit   [31:0] regW_i;
+logic [31:0] regA_o;
+logic [31:0] regB_o;
+
+int i;
+int reg_exp [31:0];
+
+
+
+initial begin
+    clk = 1'b0;
+    wen = 1'b0;
+    regA_sel = 5'b0;
+    regB_sel = 5'b0;
+    regW_sel = 5'b0;
+    #10 assert(regA_o == 0 && regB_o == 0);
+    // Initializes an array of random numbers, leaving the first element empty
+    reg_exp[0] = 0;
+    for(i = 1; i < 32; i = i + 1)
+        reg_exp[i] = $random;
+    wen = 1'b1;
+    for(i = 0; i < 32; i = i + 1) begin
+        regW_sel = i;
+        regW_i = reg_exp[i];
+        #10;
+    end
+    wen = 1'b0;
+    // Check if write happened properly through both ports
+    for(i = 0; i < 32; i = i + 1) begin
+        regA_sel = i;
+        regB_sel = i;
+        #1 assert(regA_o == reg_exp[i]) else $display("Actual: %h, Expected: %h at i=%d", regA_o, reg_exp[i], i);
+        #0 assert(regB_o == reg_exp[i]) else $display("Actual: %h, Expected: %h at i=%d", regB_o, reg_exp[i], i);
+        #8;
+    end
+    $finish;
+end
+
+always
+    #5 clk = !clk;
+
+regfile dut(
+    .clk,
+    .wen,
+    .regA_sel,
+    .regB_sel,
+    .regW_sel,
+    .regW_i,
+    .regA_o,
+    .regB_o
+);
+
+endmodule
